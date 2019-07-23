9fbfa135c92e227b57babacbca66568c3de43346
JeelsBoobz <38210868+JeelsBoobz@users.noreply.github.com>
2019-07-23 17:02:06 +0700

    Update .update.sh

--- .update.sh
+++ .update.sh
@@ -1,5 +1,27 @@
 #!/data/data/com.termux/files/usr/bin/sh
 
+
+# Use colors, but only if connected to a terminal, and that terminal
+# supports them.
+if which tput >/dev/null 2>&1; then
+    ncolors=$(tput colors)
+fi
+if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+  RED="$(tput setaf 1)"
+  GREEN="$(tput setaf 2)"
+  YELLOW="$(tput setaf 3)"
+  BLUE="$(tput setaf 4)"
+  BOLD="$(tput bold)"
+  NORMAL="$(tput sgr0)"
+else
+  RED=""
+  GREEN=""
+  YELLOW=""
+  BLUE=""
+  BOLD=""
+  NORMAL=""
+fi
+
 check_package() {
     if [ "$(dpkg-query -W -f='${Status}' "$1" 2>/dev/null | grep -c 'ok installed')" -eq 0 ];
     then
@@ -7,5 +29,19 @@ check_package() {
     fi
 }
 
-check_package coreutils
-git pull origin master
+printf "${BLUE}%s${NORMAL}\n" "Updating DarkFB Premium"
+cd "$ZSH"
+if check_package coreutils && git pull origin master
+then
+  printf '%s' "$GREEN"
+  printf '%s\n' '         __                                     __   '
+  printf '%s\n' '  ____  / /_     ____ ___  __  __   ____  _____/ /_  '
+  printf '%s\n' ' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '
+  printf '%s\n' '/ /_/ / / / /  / / / / / / /_/ /    / /_(__  ) / / / '
+  printf '%s\n' '\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/  '
+  printf '%s\n' '                        /____/                       '
+  printf "${BLUE}%s\n" "Hooray! DarkFB Premium has been updated and/or is at the current version."
+  Printf "${BLUE}${BOLD}%s${NORMAL}\n" "More information at:  https://github.com/JeelsBoobz/DarkFB"
+else
+  printf "${RED}%s${NORMAL}\n" 'There was an error updating. Try again later?'
+fi
