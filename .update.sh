0f719d64d2b692e5b7927cc7f3c546911cd5af33
JeelsBoobz <38210868+JeelsBoobz@users.noreply.github.com>
2019-07-23 18:30:34 +0700

    Update .update.sh

--- .update.sh
+++ .update.sh
@@ -27,7 +27,9 @@ check_package() {
 }
 
 printf "${BLUE}%s${NORMAL}\n" "Updating DarkFB Premium"
-if [ "$(check_package coreutils && check_package figlet && git pull origin master)" ];
+check_package coreutils
+check_package figlet
+if git pull origin master
 then
   #printf '%s' "$GREEN"
   #printf '%s\n' '         __                                     __   '
